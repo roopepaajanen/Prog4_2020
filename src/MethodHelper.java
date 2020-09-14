8971beb99b191263d2689ffa50a9650e78b10279
Stewu1 <joakim.stewen@gmail.com>
2020-09-14 17:28:19 +0300

    heelo

---
+++ src/MethodHelper.java
@@ -0,0 +1,13 @@
+public class MethodHelper {
+
+    //album information
+
+    //add and/or remove sub albums
+
+    //add and/or remove sound files to and from albums
+
+    //check if an album contains another album or a certain sound file
+
+
+}
+
